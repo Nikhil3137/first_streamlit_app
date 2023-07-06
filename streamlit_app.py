5e9e50a2bb5c75203538fdd4f6d3a08181b98d20
Nikhil3137 <132592208+Nikhil3137@users.noreply.github.com>
2023-07-06 10:44:26 +0530

    Update streamlit_app.py
    add header and some text in it for show on streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("My Parents new healthy diner")
+streamlit.header("Breakfast menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale,spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
+
