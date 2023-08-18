f1cb4cf14b255cfdfa21a9c3b80817d919294d33
Nikhil3137 <132592208+Nikhil3137@users.noreply.github.com>
2023-08-18 12:40:07 +0530

    Update HealthApp.py

--- HealthApp.py
+++ HealthApp.py
@@ -2,16 +2,4 @@ import streamlit
 from streamlit_option_menu import option_menu
 streamlit.title('HealthApp')
 
-with stremlit.sidebar:
-  selected = option_menu(
-    menu_title = "Main Menu",
-    options =["Home", "Projects","Contact"],
-  )
 
-if selected == "Home":
-  streamlit.title(f"You have selected{selected}")
-if selected == "Projeccts":
-  streamlit.title(f"You have selected{selected}")  
-if selected == "Conatcts":
-  streamlit.title(f"You have selected{selected}")  
-  
